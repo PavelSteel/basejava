c2c192daba020e7636816943ccca44a30253cd77
PavelSteel <misskiisteel@gmail.com>
2022-10-13 00:50:44 +0300

    реализация метода delete()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,13 +5,13 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i < storage.length - 1; i++) {
+        for (int i = 0; i < storage.length-1; i++) {
             storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length - 1; i++) {
+        for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
                 break;
@@ -21,8 +21,8 @@ public class ArrayStorage {
 
     Resume get(String uuid) {
         Resume g = new Resume();
-        for (int i = 0; i < storage.length - 1; i++) {
-            if (storage[i].toString() == uuid) {
+        for (int i = 0; i < storage.length-1; i++) {
+            if (storage[i].toString().equals(uuid)) {
                 g = storage[i];
             } else {
                 g = null;
@@ -32,6 +32,17 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (i != storage.length - 1) {
+                if (storage[i].toString().equals(uuid)) {
+                    for (int j = i; j < storage.length; j++) {
+                        storage[j] = storage[j + 1];
+                    }
+                }
+            } else {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
