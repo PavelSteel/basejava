579f2a3a9c5365049f89cd64b7ba87f355d50fec
PavelSteel <misskiisteel@gmail.com>
2022-11-01 23:40:57 +0300

    update ArrayStorage

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -8,17 +8,21 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
     }
 
-    void save(Resume r) {
+    public void update(Resume resume) {
+
+    }
+
+    public void save(Resume r) {
         if (size < storage.length) {
             storage[size] = r;
             size++;
@@ -27,39 +31,42 @@ public class ArrayStorage {
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
+            if (contains(i, uuid)) {
                 return storage[i];
             }
         }
         return null;
     }
 
-    //если удаляется последнее резюме. следовательно нужна проверка. что оно не последнее
-    void delete(String uuid) {
+    public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                if (i == size - 1) {
-                    storage[i] = null;
-                } else {
-                    storage[i] = storage[size - 1];
-                    storage[size - 1] = null;
-                }
+            if (contains(i, uuid)) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
-                break;
             }
         }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
+    }
+
+    private boolean contains(int i, String uuid) {
+        if (storage[i].getUuid().equals(uuid)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
