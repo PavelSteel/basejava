c802dfccbc3adff82c57242c198b5509db22cbfd
PavelSteel <misskiisteel@gmail.com>
2023-01-11 23:17:39 +0300

    Lesson04 Reflection and JUnit

---
+++ src/com/urise/webapp/MainReflection.java
@@ -0,0 +1,17 @@
+package com.urise.webapp;
+
+import com.urise.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(r));
+        field.set(r, "new_uuid");
+        System.out.println(r);
+    }
+}
