e5f28a13c640b4e7496b69bcfd874abd89dcc4f6
PavelSteel <misskiisteel@gmail.com>
2023-02-26 23:04:06 +0300

    Refactoring code

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -5,7 +5,7 @@ import com.urise.webapp.model.Resume;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class AbstractArrayStorageTest extends AbstractStorageTest{
+public abstract class AbstractArrayStorageTest extends AbstractStorageTest{
     protected AbstractArrayStorageTest(Storage storage) {
         super(storage);
     }
