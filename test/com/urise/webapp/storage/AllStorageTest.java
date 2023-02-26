0694994a5d342c4718b4b2e93fcfffce5980baae
PavelSteel <misskiisteel@gmail.com>
2023-02-26 21:31:10 +0300

    Lesson07_HW06 test refactoring

---
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -0,0 +1,14 @@
+package com.urise.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        SortedArrayStorageTest.class,
+        ListStorageTest.class,
+        MapStorageTest.class
+})
+public class AllStorageTest {
+}
