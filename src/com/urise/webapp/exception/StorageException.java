e8b43b99a719eee0b5f850a8295b9f8f9bf29b78
PavelSteel <misskiisteel@gmail.com>
2023-03-05 22:03:23 +0300

    Created AbstractFileStorage

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -1,10 +1,17 @@
 package com.urise.webapp.exception;
 
+import java.io.IOException;
+
 public class StorageException extends RuntimeException {
     private final String uuid;
 
     public StorageException(String message, String uuid) {
         super(message);
+        this.uuid = uuid;
+    }
+
+    public StorageException(String message, String uuid, IOException e) {
+        super(message, e);
         this.uuid = uuid;
     }
 
