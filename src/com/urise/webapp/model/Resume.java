c4af4854f3bc3ff0b4ecf12285bfc429340a59c0
PavelSteel <misskiisteel@gmail.com>
2023-03-06 00:06:02 +0300

    Refactoring code in model

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -32,10 +32,20 @@ public class Resume implements Comparable<Resume> {
         return uuid;
     }
 
-    public String getContact(ContactType type){
+    public String getContact(ContactType type) {
         return contacts.get(type);
-    }public Section getSection(SectionType type){
+    }
+
+    public Section getSection(SectionType type) {
         return sections.get(type);
+    }
+
+    public void addContact(ContactType type, String value) {
+        contacts.put(type, value);
+    }
+
+    public void addSection(SectionType type, Section section) {
+        sections.put(type, section);
     }
 
     @Override
