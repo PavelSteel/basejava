4842509a15378f799452c6f5d5fac605020a032f
PavelSteel <misskiisteel@gmail.com>
2023-03-08 18:17:34 +0300

    Implementation ObjectStreamPathStorageTest

---
+++ test/com/urise/webapp/storage/ObjectStreamPathStorageTest.java
@@ -0,0 +1,9 @@
+package com.urise.webapp.storage;
+
+
+public class ObjectStreamPathStorageTest extends AbstractStorageTest {
+
+    public ObjectStreamPathStorageTest() {
+        super(new ObjectStreamPathStorage(STORAGE_DIR.getAbsolutePath()));
+    }
+}
