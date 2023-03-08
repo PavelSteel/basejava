bdef470894d5fdc6aa2bbcd19a967165ec0ec6d0
PavelSteel <misskiisteel@gmail.com>
2023-03-08 18:09:53 +0300

    Fixed StorageException

--- src/com/urise/webapp/exception/StorageException.java
+++ src/com/urise/webapp/exception/StorageException.java
@@ -1,13 +1,19 @@
 package com.urise.webapp.exception;
 
-import java.io.IOException;
-
 public class StorageException extends RuntimeException {
     private final String uuid;
+
+    public StorageException(String message) {
+        this(message, null, null);
+    }
 
     public StorageException(String message, String uuid) {
         super(message);
         this.uuid = uuid;
+    }
+
+    public StorageException(String message, Exception e) {
+        this(message, null, e);
     }
 
     public StorageException(String message, String uuid, Exception e) {
