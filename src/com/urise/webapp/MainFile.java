2d1ef69a38d24033f1ed50ca7d908824b437913e
PavelSteel <misskiisteel@gmail.com>
2023-03-08 15:49:25 +0300

    HW09_indented recursive output

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -7,19 +7,19 @@ public class MainFile {
     public static void main(String[] args) throws IOException {
         File dir = new File("C:\\Users\\Вероника\\Desktop\\Topjava\\basejava");
 
-        deepPrintDirectory(dir);
+        deepPrintDirectory(dir, "");
     }
 
     //Вывод всех файлов и папок в директории
-    public static void deepPrintDirectory(File dir){
+    public static void deepPrintDirectory(File dir, String indents) {
         File[] files = dir.listFiles();
-        if(files != null){
-            for (File file : files){
-                if (file.isFile()){
-                    System.out.println("File: " + file.getName());
-                } else if (file.isDirectory()){
-                    System.out.println("Directory: " + file.getName());
-                    deepPrintDirectory(file);
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println(indents + "File: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println(indents + "Directory: " + file.getName());
+                    deepPrintDirectory(file, indents + " ");
                 }
             }
         }
