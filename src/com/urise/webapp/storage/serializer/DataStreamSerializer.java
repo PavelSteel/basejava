69c6a58bdae6473eb5636f0a334a6c6f81dfd862
PavelSteel <misskiisteel@gmail.com>
2023-03-13 00:55:17 +0300

    Add DataStreamSerializer

---
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -0,0 +1,39 @@
+package com.urise.webapp.storage.serializer;
+
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.ContactType;
+import com.urise.webapp.model.Resume;
+
+import java.io.*;
+import java.util.Map;
+
+public class DataStreamSerializer implements StreamSerializer {
+    @Override
+    public void doWrite(Resume r, OutputStream os) throws IOException {
+        try (DataOutputStream dos = new DataOutputStream(os)) {
+            dos.writeUTF(r.getUuid());
+            dos.writeUTF(r.getFullName());
+            Map<ContactType, String> contacts = r.getContacts();
+            dos.writeInt(contacts.size());
+            for (Map.Entry<ContactType, String> entry : r.getContacts().entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
+            }
+
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream io) throws IOException {
+        try (DataInputStream dis = new DataInputStream(io)) {
+            String uuid = dis.readUTF();
+            String fullName = dis.readUTF();
+            Resume resume = new Resume(uuid, fullName);
+            int size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
+            }
+            return resume;
+        }
+    }
+}
--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -12,7 +12,8 @@ import org.junit.runners.Suite;
         MapResumeStorageTest.class,
         ObjectStreamFileStorageTest.class,
         ObjectStreamPathStorageTest.class,
-        XmlPathStorageTest.class
+        XmlPathStorageTest.class,
+        JsonPathStorageTest.class
 })
 public class AllStorageTest {
 }
