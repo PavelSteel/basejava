af301a974d9a01e9f81392ec86225fdaf619a7fb
PavelSteel <misskiisteel@gmail.com>
2023-03-12 20:31:45 +0300

    Add JsonParser

---
+++ src/com/urise/webapp/util/JsonParser.java
@@ -0,0 +1,19 @@
+package com.urise.webapp.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.Reader;
+import java.io.Writer;
+
+public class JsonParser {
+    private static Gson GSON = new GsonBuilder().create();
+
+    public static <T> T read(Reader reader, Class<T> tClass) {
+        return GSON.fromJson(reader, tClass);
+    }
+
+    public static <T> void write(T object, Writer writer) {
+        GSON.toJson(object, writer);
+    }
+}
