2d564e21dc0c36cf0885f187261dda08ebd4fa82
PavelSteel <misskiisteel@gmail.com>
2023-04-08 01:19:58 +0300

    Add SqlStorage and interface

---
+++ src/com/urise/webapp/storage/SqlStorage.java
@@ -0,0 +1,75 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.exception.NotExistStorageException;
+import com.urise.webapp.exception.StorageException;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.sql.ConnectionFactory;
+
+import java.sql.*;
+import java.util.List;
+
+public class SqlStorage implements Storage {
+    public final ConnectionFactory connectionFactory;
+
+    public SqlStorage(String dbUrl, String dbUser, String dbPassword) {
+        connectionFactory = () -> DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+
+    @Override
+    public void clear() {
+        try (Connection con = connectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement("DELETE FROM resume")) {
+            ps.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public void update(Resume resume) {
+
+    }
+
+    @Override
+    public void save(Resume r) {
+        try (Connection con = connectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement("INSERT INTO resume (uuid, full_name) VALUES (?,?)")) {
+            ps.setString(1, r.getUuid());
+            ps.setString(2, r.getFullName());
+            ps.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public Resume get(String uuid) {
+        try (Connection con = connectionFactory.getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT * FROM resume r WHERE r.uuid =?")) {
+            ps.setString(1, uuid);
+            ResultSet rs = ps.getResultSet();
+            if (!rs.next()) {
+                throw new NotExistStorageException(uuid);
+            }
+            Resume r = new Resume(uuid, rs.getString("full_name"));
+            return r;
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
+    }
+
+    @Override
+    public void delete(String uuid) {
+
+    }
+
+    @Override
+    public List<Resume> getAllSorted() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
