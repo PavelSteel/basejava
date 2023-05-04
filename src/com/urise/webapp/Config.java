bf45808531aa5cafbc073097d6b0b342111a862c
PavelSteel <misskiisteel@gmail.com>
2023-05-04 19:03:59 +0300

    Refactoring Config

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -1,13 +1,16 @@
 package com.urise.webapp;
 
+import com.urise.webapp.storage.SqlStorage;
+import com.urise.webapp.storage.Storage;
+
 import java.io.*;
 import java.util.Properties;
 
 public class Config {
     private static final File PROPS = new File("C:\\Users\\Вероника\\Desktop\\Topjava\\basejava\\config\\resumes.properties");
     private static final Config INSTANCE = new Config();
-    private Properties props = new Properties();
-    private File storageDir;
+    private final File storageDir;
+    private final Storage storage;
 
     public static Config get() {
         return INSTANCE;
@@ -15,8 +18,10 @@ public class Config {
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
+            storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException e) {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
@@ -24,5 +29,9 @@ public class Config {
 
     public File getStorageDir() {
         return storageDir;
+    }
+
+    public Storage getStorage() {
+        return storage;
     }
 }
