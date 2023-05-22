110e4fda2920eab74fbab9b5e654ea46ea773783
PavelSteel <misskiisteel@gmail.com>
2023-05-22 17:15:02 +0300

    impl view with sections

--- src/com/urise/webapp/util/DateUtil.java
+++ src/com/urise/webapp/util/DateUtil.java
@@ -2,12 +2,18 @@ package com.urise.webapp.util;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
     public static final LocalDate NOW = LocalDate.of(3000, 1, 1);
-
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MM/yyyy");
 
     public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
+    }
+
+    public static String format(LocalDate date){
+        if (date == null) return "";
+        return date.equals(NOW) ? "Сейчас" : date.format(DATE_FORMATTER);
     }
 }
---
+++ src/com/urise/webapp/util/HtmlUtil.java
@@ -0,0 +1,9 @@
+package com.urise.webapp.util;
+
+import com.urise.webapp.model.Organization;
+
+public class HtmlUtil {
+    public static String formatDates(Organization.Position position){
+        return DateUtil.format(position.getStartDate()) + " - " + DateUtil.format(position.getEndDate());
+    }
+}
